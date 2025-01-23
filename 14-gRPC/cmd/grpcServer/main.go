6aaf36a41c48e522bd484dbf2bb49bf31e5af118
gianverdum <gian.verdum@gmail.com>
2025-01-23 14:27:31 -0300

    evans interation

--- 14-gRPC/cmd/grpcServer/main.go
+++ 14-gRPC/cmd/grpcServer/main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"database/sql"
 	"net"
+	"path/filepath"
 
 	"github.com/gianverdum/fullcycle_goexpert/14-gRPC/internal/database"
 	"github.com/gianverdum/fullcycle_goexpert/14-gRPC/internal/pb"
 	"github.com/gianverdum/fullcycle_goexpert/14-gRPC/internal/service"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "/db.sqlite")
+	absPath, err := filepath.Abs("./db.sqlite")
+    if err != nil {
+        panic(err)
+    }
+
+	db, err := sql.Open("sqlite3", absPath)
 	if err != nil {
 		panic(err)
 	}
--- 14-gRPC/go.mod
+++ 14-gRPC/go.mod
@@ -4,6 +4,7 @@ go 1.22.2
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.24
 	google.golang.org/grpc v1.69.4
 	google.golang.org/protobuf v1.36.3
 )
